109b8993fe7ae41ff5e7c248aa719fa66b8537ed
David <davidscully129@gmail.com>
2018-02-17 14:32:06 +0000

    Added login form styling Cleaned up the navbar Enabled all teh IE polyfills Updated the dark theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
 import {
   MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule, MatCardModule,
-  MATERIAL_SANITY_CHECKS
+  MATERIAL_SANITY_CHECKS, MatFormFieldModule, MatSelectModule, MatInputModule, MatDividerModule, ShowOnDirtyErrorStateMatcher,
+  ErrorStateMatcher
 } from '@angular/material';
 import {NavbarComponent} from './navbar/navbar.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -22,9 +23,14 @@ import {FormsModule} from '@angular/forms';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatIconModule, MatCardModule
+    ReactiveFormsModule,
+    MatButtonModule, MatCheckboxModule,
+    MatToolbarModule, MatMenuModule,
+    MatIconModule, MatCardModule,
+    MatFormFieldModule, MatSelectModule,
+    MatInputModule, MatDividerModule
   ],
-  providers: [{provide: MATERIAL_SANITY_CHECKS, useValue: false}],
+  providers: [{provide: MATERIAL_SANITY_CHECKS, useValue: false}, {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.maxLength(20), Validators.minLength(5)]],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
+  login() {
+    console.log(`Logging in`);
+    console.log(`Username: ${this.username.value}`);
+    console.log(`Password: ${this.password.value}`);
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  getUsernameErrorMessage() {
+    return this.username.hasError('required') ? 'You must enter a username' :
+      this.username.hasError('maxlength') ? 'Username cannot have more than 20 characters' :
+      this.username.hasError('minlength') ? 'Username cannot have less than 5 characters' :
+        '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a password' :
+        '';
+  }
 }
